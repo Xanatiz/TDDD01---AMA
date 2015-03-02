f0fb7710df15b7e9ad33a2c7ddb864ebbc2c1a80
Trites <gustaf.svanerud@gmail.com>
2015-03-02 14:21:53 +0100

    Cleaned up in main and switched logger output to .log so that it will be ignored in future commits.

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ def enc( text ):
 	return repr(text)
 
 def log( text ):
-	print(enc(text), file = open("log.txt", mode="a", encoding='utf-8'))
+	print(enc(text), file = open("log.log", mode="a", encoding='utf-8'))
 
 def clear():
 	open("log.txt", "w+")
--- src/main.py
+++ src/main.py
@@ -7,6 +7,3 @@ if __name__ == "__main__":
     barackPage = wikiparser.page("Barack Obama")
     logger.log(barackPage.content)
     print("See log file for result")
-
-    name = 'Christensen Sk\xf6ld'
-    print(name)
